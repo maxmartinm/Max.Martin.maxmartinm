08606b37064e5d0a75cb6f374babaca999fe6912
AlexBailey20 <arb4jr@virginia.edu>
2015-06-19 17:39:07 +0200

    Added relations between user, preferences, and categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,4 @@
 class Category < ActiveRecord::Base
+	has_many :preferences
+	has_many :users, through: :preferences
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_many :preferences
+  has_many :categories, through: :preferences
   validates(:name, :presence => true)
 end
