94d4b767940ad1e135b6519e0dccd01b03f3804b
edupugantiv <vijayedupuganti@yahoo.com>
2015-06-20 14:22:09 +0200

    Added charities MVC

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ActiveRecord::Base
 	has_many :preferences
+	has_many :charities
 	has_many :users, through: :preferences
 end
---
+++ app/models/charity.rb
@@ -0,0 +1,5 @@
+class Charity < ActiveRecord::Base
+	has_many :categories
+
+	
+end
