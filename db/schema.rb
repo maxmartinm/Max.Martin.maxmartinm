c54f5a9b6b943403cdf1b7630d8ae362d988adc5
AlexBailey20 <arb4jr@virginia.edu>
2015-06-22 15:20:34 +0200

    Added image support to categories (image must be manually added in console or seed)

--- db/schema.rb
+++ db/schema.rb
@@ -11,22 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150620150326) do
+ActiveRecord::Schema.define(version: 20150622131617) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   create_table "charities", force: :cascade do |t|
     t.string "name"
     t.text   "description"
-  end
-
-  create_table "charities_users", id: false, force: :cascade do |t|
-    t.integer "user_id",    null: false
-    t.integer "charity_id", null: false
   end
 
   create_table "classifications", force: :cascade do |t|
