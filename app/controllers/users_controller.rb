3121e8820c5f8cb086d51f6f35f0bb12f19e2b21
AlexBailey20 <arb4jr@virginia.edu>
2015-06-22 13:31:52 +0200

    User can now rotate through charity results on their show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,12 @@ class UsersController < ApplicationController
 		@user = User.find(params[:id])
 		@preferences = @user.preferences
 		@charities = @user.charities
+		@results = @user.results
+		if(params[:i] != nil)
+			@i = params[:i]
+		else
+			@i = 0
+		end
 	end
 	def welcome
 	end
