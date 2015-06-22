1ba09969df423f25dd1ef84f057b1456095add30
AlexBailey20 <arb4jr@virginia.edu>
2015-06-22 15:40:56 +0200

    A couple navigation improvements on the root page and the user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,5 +14,10 @@ class UsersController < ApplicationController
 		end
 	end
 	def welcome
+		if user_signed_in?
+			redirect_to user_path(:id => current_user.id)
+		else
+			redirect_to new_user_registration_path
+		end
 	end
 end
